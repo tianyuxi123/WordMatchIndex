5f74afdebd891b7d101680985732103bf85fa655
yourname <290778431@qq.com>
2017-06-05 11:36:16 +0800

    首次签入

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string ss = "acabaabaabcacaa";
+            string pp =      "abaabcac";
+            var arrayss = ss.ToCharArray();
+            var arraypp = pp.ToCharArray();
+            string ss1 = string.Empty;
+            for (int i = 0; i <= arrayss.Length-arraypp.Length; i++)
+            {
+                if(i==5)
+                     ss1 = string.Empty;
+                bool IsOk = true;
+                for (int j = 0; j < arraypp.Length; j++)
+                {
+                    if (arrayss[i + j] != arraypp[j])
+                    {
+                        IsOk = false;
+                        break; ;
+                    }
+                }
+                if (IsOk)
+                {
+                    Console.Write(i + 1);
+                    break;
+                }
+            }
+            Console.ReadLine();
+         
+        }
+    }
+}
